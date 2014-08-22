9842b167a52ec155bdb8c7fc24447c6cdef1d3d0
Fabian Vogelsteller <fabian@frozeman.de>
2014-08-22 19:48:54 +0200

    update template session

--- TemplateSession.js
+++ TemplateSession.js
@@ -50,8 +50,8 @@ TemplateSession = {
                 throw new Error('TemplateSession works only from withing template helpers, hooks or events');
         }
 
-        // move on view up if its a #with
-        while(template.kind === 'Spacebars_with' && template.parentView) {
+        // move on view up if its a #with, #if or #unless
+        while((template.kind === 'Spacebars_with' || template.kind === 'if' || template.kind === 'unless') && template.parentView) {
             template = template.parentView;
         }
 
